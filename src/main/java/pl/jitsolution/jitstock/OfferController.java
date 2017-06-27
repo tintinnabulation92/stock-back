9d0885e56c4f3ab38060a42ca9096ab69948224a
Paweł Zawadzki <pawel.zawadzki@jitsolutions.pl>
2017-06-27 13:48:45 +0200

    fully initial app

--- src/main/java/pl/jitsolution/jitstock/OfferController.java
+++ src/main/java/pl/jitsolution/jitstock/OfferController.java
@@ -1,9 +1,7 @@
 package pl.jitsolution.jitstock;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,7 +15,6 @@ public class OfferController {
 
     @RequestMapping("/api/offers")
     public List<Offer> getAllOffers() {
-        int integer =5;
         List<Offer> offerList = new ArrayList<>();
         offerRepository.findAll().forEach(offerList::add);
         return offerList;
