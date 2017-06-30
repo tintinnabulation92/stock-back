3cbd62dcb9557425de43bfd3b567cba46245b291
Maciej Dziamalek <maciej.dziamalek@jitsolutions.com>
2017-06-30 11:29:47 +0200

    pattern imp

--- src/main/java/pl/jitsolution/jitstock/controller/OfferController.java
+++ src/main/java/pl/jitsolution/jitstock/controller/OfferController.java
@@ -1,8 +1,10 @@
-package pl.jitsolution.jitstock;
+package pl.jitsolution.jitstock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.jitsolution.jitstock.entity.Offer;
+import pl.jitsolution.jitstock.repository.OfferRepository;
 
 import java.util.ArrayList;
 import java.util.List;
